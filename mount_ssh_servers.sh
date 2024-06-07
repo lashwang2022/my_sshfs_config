ba1dbc4ba97c34e9961b30fbcae59ea5d07f13a0
zhengwang <zhengwang@grandstream.cn>
2024-06-07 09:58:05 +0800

    remove mout 241 server

--- mount_ssh_servers.sh
+++ mount_ssh_servers.sh
@@ -2,6 +2,6 @@
 
 OPT="-o allow_other -o follow_symlinks "
 sshfs $OPT zhengwang@192.168.120.31:/media/4T2/zhengwang/ /mnt/192.168.120.31/
-sshfs $OPT simon@192.168.124.241:/data/ /mnt/192.168.124.241/
+#sshfs $OPT simon@192.168.124.241:/data/ /mnt/192.168.124.241/
 
 
