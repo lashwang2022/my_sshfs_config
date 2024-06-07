f9e7ef6b9bbd5412d202e425afccbf1bc7a12a67
zhengwang <zhengwang@grandstream.cn>
2024-06-07 09:54:22 +0800

    add ssh server for 31

--- mount_ssh_servers.sh
+++ mount_ssh_servers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 OPT="-o allow_other -o follow_symlinks "
-sshfs $OPT zhengwang@192.168.120.32:/data/zhengwang /mnt/192.168.120.32/ 
-sshfs $OPT zhengwang@192.168.120.166:/media/gshz/4Tdisk/zhengwang/ /mnt/192.168.120.166/
+sshfs $OPT zhengwang@192.168.120.31:/media/4T2/zhengwang/ /mnt/192.168.120.31/
+sshfs $OPT simon@192.168.124.241:/data/ /mnt/192.168.124.241/
 
 
